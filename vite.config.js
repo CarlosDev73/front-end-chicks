93563aa57edf1d47abe8c198c6513665558f3618
Carlos Gutierrez <devcarlosg@gmail.com>
2024-11-04 00:32:35 -0400

    adding live page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://CarlosDev73.github.io/front-end-chicks',
 })
